7ed57c613febef669ec88c9fa2ca270b3fdbaf05
Johann Poon <johannpoon@Johanns-MacBook-Pro.local>
2025-02-11 17:42:04 +0800

    Added Spring Boot starter app

---
+++ src/main/java/com/trading/platform/simple_trading_app/SimpleTradingAppApplication.java
@@ -0,0 +1,13 @@
+package com.trading.platform.simple_trading_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleTradingAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleTradingAppApplication.class, args);
+	}
+
+}
